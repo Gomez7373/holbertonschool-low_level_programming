c910d22268973a1c8e42afe730f403f77f06fa5f
Gomez7373 <sgc.holberton@gmail.com>
2023-10-14 18:12:07 -0400

    5-printf.c

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - It prints a text.
+ *
+ * Description: 'The program will ptint a text.'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+("\"words");
+return (0);
+	
