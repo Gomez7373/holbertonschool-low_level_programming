605d394487c2bc8d1c21ddc18dd5d29a00c6fb57
Gomez7373 <sgc.holberton@gmail.com>
2023-10-14 19:10:31 -0400

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main prints mult. type of size on pc 
+ * 
+ * Description: 'The program will print milt. sizes'
+ *
+ *Return: Always 0 (Success)
+ */
+int main (void)
+{
+	char s;
+	int h;
+	long int e; 
+        long long int i; 
+        float l;	
+
+        printf("Size of a char: %d byte(s)",sizeof(s))
+	       printf("",sizeof(h))
+		      printf("",sizeof(e))
+		            printf("",sizeof(i))
+			         Print("",sizeof(l))
+/**
+ * a
+ */
+	return (0);
+}	
