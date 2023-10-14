14e4da9e4c571e558f6487f4c1173e5057c24da0
Gomez7373 <sgc.holberton@gmail.com>
2023-10-14 19:56:11 -0400

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,11 @@ long int e;
 long long int i; 
 float l;
 
-printf("Size of a char: %s byte(s)\n", sizeof(s));
+printf("Size of a char: %d byte(s)\n", sizeof(s));
 printf("Size of an int: %d byte(s)\n", sizeof(h));
-printf("Size of a long int: %ld byte(s)\n", sizeof(e));
-Printf("Size of a long long int: %lld byte(s)\n", sizeof(i));
-printf("Size of a float: %lf byte (S)\n", sizeof(l));
+printf("Size of a long int: %d byte(s)\n", sizeof(e));
+Printf("Size of a long long int: %d byte(s)\n", sizeof(i));
+printf("Size of a float: %d byte (S)\n", sizeof(l));
 				   
 return (0);
 }	
