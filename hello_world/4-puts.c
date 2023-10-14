b0e9ab8a3bd63a2de01e3bae18cb36daa0f2ef55
Gomez7373 <sgc.holberton@gmail.com>
2023-10-14 17:23:12 -0400

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- * main - pnt text
- *
- * Return : 0 (SUCCESS)
- */
+* main - pnt text
+*
+* Return : 0 (SUCCESS)
+*/
 int main(void)
 {
 puts("Programing is like building a multilingual puzzle\n")
-;return (0);
+; return (0);
 }
 
