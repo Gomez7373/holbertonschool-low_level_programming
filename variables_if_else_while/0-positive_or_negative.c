dbd078db15ddb2f8fd4aed02525c66eb206c4f7a
Gomez7373 <sgc.holberton@gmail.com>
2023-10-16 14:11:14 -0400

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,14 +14,14 @@ n = rand() - RAND_MAX/2;
 printf("%d \n", n);
 if(n>0)
 {
-printf=("%d is positive\n",n)
+printf=("%d is positive\n", n)
 }
 {
 printf("%d is negative\n", n);
 }
 else
 {
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 }
 return (0);
 }
