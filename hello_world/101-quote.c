1088503c2e23125f2e2bcc9cb01c2e489432b62d
Gomez7373 <sgc.holberton@gmail.com>
2023-10-15 20:30:27 -0400

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -5,6 +5,6 @@
 */
 int main(void)
 {
-fwrite(2, "and that oice of art is usful\n" - "Dora Korpar, 2015-10-19\n");
+fwrite(2, "and that oice of art is usful\n" - "Dora Korpar, 2015-10-19\n",15);
 return (1);
 }
