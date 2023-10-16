ce29497ce2ad49a7d25504591893f679c6e5e8db
Gomez7373 <sgc.holberton@gmail.com>
2023-10-15 21:12:38 -0400

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<unistd.h>
 /**
-* main -  prints text
+* main -  prints "It will print a text" of Dora Korpar
 * return: Always 1.
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
