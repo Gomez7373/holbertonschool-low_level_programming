3d621fd9e482fa97cf320303049ca258906a1069
Gomez7373 <sgc.holberton@gmail.com>
2023-10-15 20:48:00 -0400

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 write(2,
-"and that pice of art is usful\n" - Dora Korpar, 2015 - 10 - 19\n",
+"and that pice of art is usful\" - Dora Korpar, 2015 - 10 - 19\n",
 15);
 return (1);
 }
