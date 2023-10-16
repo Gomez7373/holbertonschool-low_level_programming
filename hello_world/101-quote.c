a37523bb15a7e97360cc0964c21c395eaa53d9a3
Gomez7373 <sgc.holberton@gmail.com>
2023-10-15 20:46:31 -0400

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
 * main- prints text
-* Return: Always 1 .
+* Return: Always 1.
 */
 int main(void)
 {
-fwrite(2, "and that oice of art is usful\n" - "Dora Korpar, 2015-10-19\n",15);
+write(2,
+"and that pice of art is usful\n" - Dora Korpar, 2015 - 10 - 19\n",
+15);
 return (1);
 }
