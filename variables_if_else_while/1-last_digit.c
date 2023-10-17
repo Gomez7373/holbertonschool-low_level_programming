a12ae0beb8af8c8d317e8aa09cb649f5e6f313db
Gomez7373 <sgc.holberton@gmail.com>
2023-10-16 20:15:28 -0400

    0-positive_or_negative.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/**
+ * main - code gives random numbers to the variable n
+ *
+ */
 
 /*betty style doc for function main goes there */
 int main(void)
@@ -9,7 +12,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
+	if (n > 0)
+{
+	printf("%d is" /*contineu the code here*/
+}
 
 
 	return (0);
