4854a58901078b1ca94535c7999719934166af4f
Gomez7373 <sgc.holberton@gmail.com>
2023-10-17 13:51:13 -0400

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<>
+#include<>
+#include<>
+/**
+ * main - Lower case prints.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+	char start = 'a';
+	char end = 'z';
+
+	for (char letter = start; letter <= end; letter++)
+{
+	putchar('\n');
+	return 0;
+}
+	
