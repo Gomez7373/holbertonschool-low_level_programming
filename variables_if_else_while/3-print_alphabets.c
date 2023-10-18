1e7d9a4a68676012fa581bc64549e1bbf93812b7
Gomez7373 <sgc.holberton@gmail.com>
2023-10-18 18:37:28 -0400

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - The program will print lower case and then upper case.
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int s;
+	int g;
+
+	for (s = 0; s < 26; s++)
+{
+	putchar('a' + s);
+}	
+	for (g = 0; g < 26; g++)
+{
+	putchar('A' + g);
+}
+	putchar('\n');
+
+	return (0);
+}
