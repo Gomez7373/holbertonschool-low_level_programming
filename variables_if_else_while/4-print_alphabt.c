be77e00814b70f2c778d91875f54e526d1f52df3
Gomez7373 <sgc.holberton@gmail.com>
2023-10-18 19:24:35 -0400

    3-print_alphabets.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main- This program will print lower case and exclude q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+int s;
+char line[] = "excludes q and e";
+
+for (s = 0; s < 24; s++)
+{
+	putchar('a' + s);
+}
+if (line[i] != 'e' && line[i] != 'y' )
+{
+	putchar("%c", line[i]), \n;
+}
+return (0);
+}
