11e5ba5f05abbd4e2a3e8ec86ac36908b57aced0
Gomez7373 <sgc.holberton@gmail.com>
2023-10-19 13:06:00 -0400

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - program prints from 0 to 10.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+{
+	printf("%d", s);
+}
+	putchar('\n');
+	return (0);
+}
