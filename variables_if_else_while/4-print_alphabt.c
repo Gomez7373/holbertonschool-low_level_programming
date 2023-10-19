c65fa2d83fb8f234214d8a13cb8ae774ba1dc9ef
Gomez7373 <sgc.holberton@gmail.com>
2023-10-18 20:32:34 -0400

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main- This program will print lower case and exclude y  and e
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
+	int s;
 for (int s = 0; s < 26; s++)
 {
 if ('a' + s != 'e' && 'a' + s != 'y')
