acb246431815e23f3f58a44996fa06c2f72b7daa
Gomez7373 <sgc.holberton@gmail.com>
2023-10-19 14:16:26 -0400

    8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-    char hex_digits[] = "0123456789abcdef";
-    int s;
-    for (s = 0; s < 16; ++s)
-    {
-        putchar(hex_digits[s]);
-    }
-
-    putchar('\n');
-
-    return 0;
+char hex_digits[] = "0123456789abcdef";
+int s;
+for (s = 0; s < 16; ++s)
+{
+putchar(hex_digits[s]);
+}
+putchar('\n');
+return (0);
 }
