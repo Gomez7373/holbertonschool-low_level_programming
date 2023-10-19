63dbfbac028d45f6b7a9e8a3ea8ec861f3dbd783
Gomez7373 <sgc.holberton@gmail.com>
2023-10-19 14:11:41 -0400

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char hex_digits[] = "0123456789abcdef";
+
+    for (int s = 0; s < 16; ++s)
+    {
+        putchar(hex_digits[s]);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
