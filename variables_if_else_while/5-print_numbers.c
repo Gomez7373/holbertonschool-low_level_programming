cb58dec86d7d83d56fb8b21cbaad8eb44c3eb887
Gomez7373 <sgc.holberton@gmail.com>
2023-10-19 12:19:53 -0400

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*
+* main - prints all numbers of base 10 starting from 0, followed by a new line
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int s;
+
+	for (s = 0; s < 10; s++)
+{
+	pritnf("%d", s);
+}
+	putchar('\n');
+	return (0);
+}
