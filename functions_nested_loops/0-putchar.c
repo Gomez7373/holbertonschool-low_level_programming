b2ef9eb685328b50f4c793be92cc34cc303db1dc
Gomez7373 <sgc.holberton@gmail.com>
2023-10-20 06:35:48 -0400

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+
+void _putchar(char c);
+
 /**
-* main - program that prints _putchar, followed by a new line.
-*
-* Return: Always 0 (Success.)
-*/
+ * main - program that prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (Success.)
+ */
 
 int main(void)
 {
-
-	printf("_putchar"), "c";
-
-
-	printf('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
+}
+// void _putchar
+void _putchar(char c)
+{
+putchar(c);
 }
