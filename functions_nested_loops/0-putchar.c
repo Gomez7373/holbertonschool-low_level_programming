d367bf6de49b2377bc00beb10242966dd30fa7ae
Gomez7373 <sgc.holberton@gmail.com>
2023-10-20 06:51:02 -0400

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -10,22 +10,15 @@ void _putchar(char c);
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-return (0);
-}
-/**
- * void _putchar
- */
-void _putchar(char c)
-{
-putchar(c);
+    return (0);
 }
