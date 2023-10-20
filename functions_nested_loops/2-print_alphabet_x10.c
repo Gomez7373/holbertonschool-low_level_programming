61f26f5246b9a4c62fd5d1592f5d8949ef1901d1
Gomez7373 <sgc.holberton@gmail.com>
2023-10-20 15:29:06 -0400

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - it prints the alphabet x10.
+ *
+ * Return - void
+ */
+
+void print_alphabet_x10(void);
+
+{
+char s;
+for (s = 'a'; s <= 'z'; s++)
+	while (s< 11 );
+{
+_putchar(s);
+}
+
+_putchar('\n');
+}
