c90845298577acfeebc205431a995652709f9082
Gomez7373 <sgc.holberton@gmail.com>
2023-10-21 17:54:43 -0400

    0-isupper.c

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,35 +1,16 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * _isupper - Checks if a character is uppercase.
- * @c: The character to be checked.
+ * main - check the code for Holberton School students.
  *
- * Return: 1 if c is uppercase, 0 otherwise.
+ * Return: Always 0.
  */
-int _isupper(int c)
-{
-    return (c >= 'A' && c <= 'Z');
-}
-
 int main(void)
 {
-    char ch;
+	char c;
 
-    printf("Check all uppercase characters (from 'A' to 'Z')\n");
-    for (ch = 'A'; ch <= 'Z'; ch++)
-    {
-        if (_isupper(ch))
-            printf("Uppercase character: %c\n", ch);
-    }
-
-    printf("\nCheck all characters except uppercase characters (From 32 to 126 in the ASCII table)\n");
-    for (ch = 32; ch <= 126; ch++)
-    {
-        if (!_isupper(ch))
-            printf("Not an uppercase character: %c\n", ch);
-    }
-
-    return (0);
+	for (c = 'A'; c <= 'Z'; ++c)
+		printf("%c: %d\n", c, _isupper(c));
+	return (0);
 }
-
