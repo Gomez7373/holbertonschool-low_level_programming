526cf8f643e331c670d0138c2ed52cac2bf510c2
Gomez7373 <sgc.holberton@gmail.com>
2023-10-21 10:08:54 -0400

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,36 +5,34 @@
  */
 void times_table(void)
 {
-    int row, col, result;
+	int row, col, result;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            result = row * col;
+	for (row = 0; row <= 9; row++)
+{
+	for (col = 0; col <= 9; col++)
+{
+	result = row * col;
 
-            if (col == 0)
-                _putchar(result + '0');
-            else
-            {
-              
-	        _putchar(' ');	
-	       
+	if (col == 0)
+	_putchar(result + '0');
+	else
+{
+	_putchar(' ');
 
-                if (result < 10)
-                    _putchar(' ');
+	if (result < 10)
+	_putchar(' ');
 
-                else
-                    _putchar((result / 10) + '0');
+	else
+	_putchar((result / 10) + '0');
 
-                _putchar((result % 10) + '0');
-            }
+	_putchar((result % 10) + '0');
+}
 
-            if (col < 9)
-                _putchar(',');
-            else
-                _putchar('\n');
-        }
-    }
+	if (col < 9)
+	_putchar(',');
+	else
+	_putchar('\n');
+}
+}
 }
 
