764836577766fde4786d8fdb41b34d011f26e050
Gomez7373 <sgc.holberton@gmail.com>
2023-10-21 21:47:57 -0400

    9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 /**
  * 9-fizz_buzz - filter out the 99.5% of programming job candidates
  *
- * main - prints the numb from 1-100.
+ * int main(void) - prints the numb from 1-100.
  *
  * Return: Always 0 (Success)
  */
