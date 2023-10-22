599c8591b3dcdafd9a2fa6407e9e55720f671749
Gomez7373 <sgc.holberton@gmail.com>
2023-10-21 21:22:47 -0400

    6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -6,20 +6,20 @@
  */
 void print_line(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int i;
+	if (n <= 0)
+{
+	_putchar('\n');
+}
+	else
+{
+	int i;
 
-        for (i = 0; i < n; i++)
-        {
-            _putchar('_');
-        }
+	for (i = 0; i < n; i++)
+{
+	_putchar('_');
+}
 
-        _putchar('\n');
-    }
+	_putchar('\n');
+}
 }
 
