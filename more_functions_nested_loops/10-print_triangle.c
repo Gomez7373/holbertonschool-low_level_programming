e477d480a6be3230f191db718eb0f13135902a8a
Gomez7373 <sgc.holberton@gmail.com>
2023-10-21 21:59:40 -0400

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,28 +6,27 @@
  */
 void print_triangle(int size)
 {
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
 
-        for (i = 0; i < size; i++)
-        {
-            for (j = 0; j < size - i - 1; j++)
-            {
-                _putchar(' ');
-            }
-
-            for (j = 0; j < i + 1; j++)
-            {
-                _putchar('#');
-            }
-
-            _putchar('\n');
-        }
-    }
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+_putchar(' ');
 }
 
+for (j = 0; j < i + 1; j++)
+{
+_putchar('#');
+}
+
+_putchar('\n');
+}
+}
+}
