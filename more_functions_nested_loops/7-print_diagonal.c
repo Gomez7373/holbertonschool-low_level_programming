b8b6d08352016c8c8f76b64424e7a8b36e070c44
Gomez7373 <sgc.holberton@gmail.com>
2023-10-21 21:30:13 -0400

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,23 @@
  */
 void print_diagonal(int n)
 {
-    if (n <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        int i, j;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
 
-        for (i = 0; i < n; i++)
-        {
-            for (j = 0; j < i; j++)
-            {
-                _putchar(' ');
-            }
-
-            _putchar('\\');
-            _putchar('\n');
-        }
-    }
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
 }
 
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
