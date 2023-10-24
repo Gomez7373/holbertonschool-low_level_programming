782dccb29250b5006ef9b40b14e7587e9822cce0
Gomez7373 <sgc.holberton@gmail.com>
2023-10-24 13:36:48 -0400

    0-reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* reset_to_98 - (int *n) function takes a pointer to an int 
-* 
+* reset_to_98 - (int *n) function takes a pointer to an int
+*
 * @n: updates value
 *
 * Return: void
