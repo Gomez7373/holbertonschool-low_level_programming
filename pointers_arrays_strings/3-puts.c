5493d673fbc1eb38e548f7f1fdbb28cb3de0b60b
Gomez7373 <sgc.holberton@gmail.com>
2023-10-25 19:50:26 -0400

    3-puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ */
+void _puts(char *str);
+
+{
+	(
+	 int main(void)
+	 )
+	char string[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	
+	}
+return (0);
+	
+}
