9ff1056d460982dd4b46a7adfd027f716f218b70
Gomez7373 <sgc.holberton@gmail.com>
2023-10-26 14:23:08 -0400

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -10,17 +10,21 @@ void print_rev(char *s)
 	int i;
 	int length;
 	s = 0; 
-	length =0;
+
+	length = 0;
 
     if (s == NULL)
+    {
         return;
-
+    }
     while (s[length] != '\0')
-        length++;
-
+    {
+	    length++;
+    }
     for (i = length - 1; length >= 0; i--)
-        putchar(s[i]);
-
+    {
+	    putchar(s[i]);
+    }
     putchar('\n');
    
 }
