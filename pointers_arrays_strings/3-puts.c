756fccbd2f8037a5acd0d27759496de7bed650e2
Gomez7373 <sgc.holberton@gmail.com>
2023-10-25 20:48:45 -0400

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -4,12 +4,10 @@
  * _puts - prints a string followed by a new line
  * @str: the string to be printed
  */
-void _puts(char *str)
-{
+void _puts(char *str) {
     while (*str != '\0') {
-        _putchar(*str);
+        putchar(*str);
         str++;
     }
-    _putchar('\n');
+    putchar('\n');
 }
-
