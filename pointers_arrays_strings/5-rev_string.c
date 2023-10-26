7137bba00fb745fae697bf49b44d9a95b12fca6b
Gomez7373 <sgc.holberton@gmail.com>
2023-10-26 17:31:30 -0400

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,3 +1,45 @@
 #include "main.h"
 #include <stdio.h>
-#include <>
+#include <string.h> 
+
+void rev_string(char *s)
+{
+int c;
+int g;
+
+g = strlen(s)
+
+	for (c = g -1; c >= 0; c--)
+	{
+		putchar(s[c]);
+	}
+
+}
+/**
+ *
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+ comment area below
+ * print_rev - prints a string in reverse, followed by a new line
+ * @s: the string to be printed
+ 
+ _______________________________
+void print_rev(char *s)
+{
+int i;
+int length;
+
+length = strlen(s);
+
+for (i = length - 1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+
+}
+_______________________________
+ */
+
