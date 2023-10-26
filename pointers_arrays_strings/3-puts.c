a2c8654437d53907997091e270ccc822966dde01
Gomez7373 <sgc.holberton@gmail.com>
2023-10-25 20:24:48 -0400

    3-puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -5,16 +5,15 @@
 void _puts(char *str)
 
 {
-	char *str
 
 	while (*str != '\0')
-	{
+{
 		putchat(*str);
 		str++;
 }
-
 	putchar('\n');
-}			
+}	
+
 	 int main(void);
 {
 	char string[] = "I do not fear computers. I fear the lack of them - Isaac Asimov";
@@ -23,4 +22,4 @@ void _puts(char *str)
 
 return (0);
 	
-}
+
