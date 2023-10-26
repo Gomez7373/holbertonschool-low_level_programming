c7ed5cae4b955a51021b393e04bda6746d257a3b
Gomez7373 <sgc.holberton@gmail.com>
2023-10-26 10:13:32 -0400

    4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,3 +1,5 @@
+/* 4-print_rev.c */
+
 #include <stdio.h>
 #include "main.h"
 
@@ -10,19 +12,20 @@ void print_rev(char *s)
     if (s == NULL)
         return;
 
-    int length = 0;
-    int i;
-    
+    int length = 0; // Declare the variable at the beginning
+
+    /* Calculate the length of the string */
     while (s[length] != '\0') {
         length++;
     }
 
-    
+    int i; // Move the declaration here
+
+    /* Print the string in reverse */
     for (i = length - 1; i >= 0; i--) {
         putchar(s[i]);
     }
 
-    
+    /* Print a new line */
     putchar('\n');
 }
-
