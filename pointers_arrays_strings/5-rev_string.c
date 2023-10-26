6c8a191e98aa306b64dceb247fc04ff59eaa49ec
Gomez7373 <sgc.holberton@gmail.com>
2023-10-26 18:36:00 -0400

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,24 +1,29 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h> 
-
+/**
+ *
+ *rev_string - it will revers my string
+ *
+ *@s: takes my string
+ */
 void rev_string(char *s)
 {
-	int g, c;
-	char temp;
+int g, c;
+char temp;
 
-	c = 0;
-	g = 0;
+c = 0;
+g = 0;
 	
 
-	while (s[c] != '\0')
-		c++;
-	c--;
-	
-	for (g = 0; g < c; c--, g++)
-	{
-	temp = s[g];
-	s[g] = s[c];
-	s[c] = temp;
-	}
+while (s[c] != '\0')
+c++;
+c--;
+
+for (g = 0; g < c; c--, g++)
+{
+temp = s[g];
+s[g] = s[c];
+s[c] = temp;
+}
 }
