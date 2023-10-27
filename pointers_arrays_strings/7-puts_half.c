e04b2a3971c9973ac26d3bba56612dac6109b20e
Gomez7373 <sgc.holberton@gmail.com>
2023-10-27 01:08:46 -0400

    7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,7 +6,7 @@ void puts_half(char *str)
 {
     int length = strlen(str);
     int start_index = (length + 1) / 2;
-
+    
     for (int i = start_index; i < length; i++)
     {
         putchar(str[i]);
