0b228174cb70c6353ae37ca1cacbc9b04468d6e2
Gomez7373 <sgc.holberton@gmail.com>
2023-10-26 22:28:28 -0400

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+
 void puts2(char *str)
 {
 	int g;
 	
 	g = 0;
 
-    for (g = 0; str[g]; g++)
+    for (g = 0; str[g]; g += 2)
     {
         putchar(str[g]);
 
