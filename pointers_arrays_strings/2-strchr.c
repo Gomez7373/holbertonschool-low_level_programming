4ecd317f8f1c2f851dfb95915ab5e0e0c491409a
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 19:52:49 -0400

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,15 +9,21 @@
  * Return: A pointer to the first occurrence of the character c in the string s,
  *         or NULL if the character is not found.
  */
+
 char *_strchr(char *s, char c)
 {
+    char *found = NULL;
+
     while (*s != '\0')
     {
         if (*s == c)
-            return s;
+        {
+            found = s;
+            break;
+        }
+
         s++;
     }
 
-    return NULL;
+    return found;
 }
-
---
+++ pointers_arrays_strings/main_0.c
@@ -0,0 +1,14 @@
+/* main_0.c */
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "First, solve the problem. Then, write the code.";
+    char *f;
+
+    f = _strchr(s, '\0');
+    printf("%s\n", (f == NULL ? "nil" : f));
+    return (0);
+}
+
