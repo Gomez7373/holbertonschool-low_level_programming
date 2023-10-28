171d9accf1b38ba1fa502561726358843e5c09ed
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 19:23:47 -0400

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,20 @@
  * Description: This function returns a pointer to the first occurrence
  * of the character c in the string s, or NULL if the character is not found.
  *
- * Return: A pointer to the first occurrence of the character c in the string s,
+ * Return: pointer to the first occurrence of the character c in the string s,
  *         or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
-while (*s != '\0') {
-if (*s == c) {
+while (*s != '\0')
+{
+if (*s == c)
+{
 return s;
 }
 s++;
 }
 
-return NULL;
+return (NULL);
 }
 
