808a355760b6f4ed194952176cf342c2bf8340ad
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 19:21:45 -0400

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -14,13 +14,13 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0') {
-        if (*s == c) {
-            return s;
-        }
-        s++;
-    }
+while (*s != '\0') {
+if (*s == c) {
+return s;
+}
+s++;
+}
 
-    return NULL;
+return NULL;
 }
 
