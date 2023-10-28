985113657d732c5c507b982f4812d882745d4013
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 08:43:48 -0400

    9-strcpy.c

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,51 +3,44 @@
 #include <limits.h>
 
 /**
- * _atoi - Converts a string to an integer.
+ * _atoi - Converts a string to a long long integer.
  * @str: The input string.
  *
  * Description: This function converts the initial portion of the string pointed
- * to by str to its equivalent integer representation.
+ * to by str to its equivalent long long integer representation.
  *
- * Return: The converted integer.
+ * Return: The converted long long integer.
  */
 
-int _atoi(char *str)
+long long _atoi(char *str)
 {
-    int result = 0;
+    long long result = 0;
     int sign = 1;
 
-   
+    // Skip leading whitespaces
     while (*str == ' ')
         str++;
 
-    
+    // Handle sign
     if (*str == '-' || *str == '+')
     {
         sign = (*str == '-') ? -1 : 1;
         str++;
     }
 
-    
+    // Convert digits to long long integer
     while (*str >= '0' && *str <= '9')
     {
-       
-        if (result > INT_MAX / 10 || (*str - '0') > (INT_MAX - result * 10))
+        // Check for overflow before the multiplication and addition
+        if (result > LLONG_MAX / 10 || (*str - '0') > (LLONG_MAX - result * 10))
         {
-            
+            // Handle overflow, e.g., print an error message or set result to a default value
+            // and break out of the loop.
             break;
         }
 
         result = result * 10 + (*str - '0');
         str++;
-    }
-
-    // Check for overflow after the conversion
-    if (result == INT_MIN && sign == -1)
-    {
-        // Handle overflow for the specific case of INT_MIN
-        // e.g., print an error message or set result to a default value
-        return result;
     }
 
     // Multiply by sign to include the sign
