ef5c6b601f15d4a9c9e37bc06e226479ea5cd8d5
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 19:16:31 -0400

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdio.h>
+
 /**
  * _strchr - Locates a character in a string.
  * @s: Pointer to the string.
  * @c: Character to be located.
  *
  * Description: This function returns a pointer to the first occurrence
- * of the character c in the string s, or NULL if the character is not found.
+ * of the character c in the string s, or "nil" if the character is not found.
  *
  * Return: A pointer to the first occurrence of the character c in the string s,
- *         or NULL if the character is not found.
- *
+ *         or "nil" if the character is not found.
  */
-
 char *_strchr(char *s, char c)
 {
     char *found = NULL;
@@ -27,5 +26,18 @@ char *_strchr(char *s, char c)
         s++;
     }
 
-    return found;
+    return (found != NULL) ? found : "nil";
 }
+
+int main(void)
+{
+    char s[] = "Hello, World!";
+    char c = 'z';
+
+    char *result = _strchr(s, c);
+
+    printf("%s\n", result);
+
+    return 0;
+}
+
