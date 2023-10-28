6de97d10e75ff63e6b4df614cca66c3eb9cdb476
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 15:01:53 -0400

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,21 +10,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    char *result = dest;
+char *result = dest;
 
-    while (*dest != '\0')
-        dest++;
+while (*dest != '\0')
+dest++;
 
-    while (*src != '\0' && n > 0)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
+while (*src != '\0' && n > 0)
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
 
-    *dest = '\0';
+*dest = '\0';
 
-    return result;
+return result;
 }
 
