15ed83a08a79684ecdfdd2c289f70ed8335e10ae
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 16:05:22 -0400

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -16,17 +16,13 @@ char *cap_string(char *str)
     while (*ptr)
     {
         if ((*ptr >= 'a' && *ptr <= 'z') || (*ptr >= 'A' && *ptr <= 'Z'))
-        {
-            *ptr = (capitalize) ? (*ptr & ~32) : (*ptr | 32);
-            capitalize = 0;
-        }
-        else if (*ptr == ' ' || (*ptr >= '\t' && *ptr <= '\n') ||
-                 *ptr == ',' || *ptr == ';' || *ptr == '.' ||
-                 *ptr == '!' || *ptr == '?' || *ptr == '\"' ||
-                 (*ptr >= '(' && *ptr <= '}'))
-        {
+            *ptr ^= capitalize ? 32 : 0;
+        else if ((*ptr >= ' ' && *ptr <= '/') || (*ptr >= ':' && *ptr <= '@') ||
+                 (*ptr >= '[' && *ptr <= '`') || (*ptr >= '{' && *ptr <= '}'))
             capitalize = 1;
-        }
+        else
+            capitalize = 0;
+
         ptr++;
     }
 
