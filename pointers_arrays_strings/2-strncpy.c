9b97f21522703e3b4257e8f6fb3df4b76338937a
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 15:25:54 -0400

    2-strncpy.c

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,7 +8,7 @@
  *
  * Return: A pointer to the resulting string dest.
  */
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 char *result = dest;
 
