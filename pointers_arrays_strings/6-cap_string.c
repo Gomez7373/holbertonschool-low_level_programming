c086456f691b83cb2c4929a1ccfdbcafd2b0fd37
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 16:30:24 -0400

    6-cap_string.c

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,6 @@ char *cap_string(char *str)
 {
     char *ptr = str;
 
-    // Capitalize the first letter of the string
     if (*ptr >= 'a' && *ptr <= 'z')
         *ptr = (*ptr & ~32);
 
@@ -24,7 +23,7 @@ char *cap_string(char *str)
             *ptr == '(' || *ptr == ')' || *ptr == '{' ||
             *ptr == '}')
         {
-            // If the next character is a lowercase letter, capitalize it
+           
             if (*(ptr + 1) && (*(ptr + 1) >= 'a' && *(ptr + 1) <= 'z'))
                 *(ptr + 1) = (*(ptr + 1) & ~32);
         }
