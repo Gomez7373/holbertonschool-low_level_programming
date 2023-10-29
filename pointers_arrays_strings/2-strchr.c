06dd72e7b1981cc8c0f905a863b7cc7e9c0ff3cd
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 21:08:26 -0400

    3-strspn.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stddef.h>
-
+#include <string.h>
 /**
  * _strchr - Locates a character in a string.
  * @s: Pointer to the string.
@@ -11,6 +11,7 @@
  */
 
 char *_strchr(char *s, char c)
+
 {
 char *found = NULL;
 
