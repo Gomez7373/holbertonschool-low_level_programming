e13ddfc73eb858a1dcc0412f15aa48715c31aa8d
Gomez7373 <sgc.holberton@gmail.com>
2023-10-28 21:50:56 -0400

    7-print_chessboard.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
  *
  * Return: pointer to the first occurrence of the character c in the string s,
  *         or NULL if the character is not found.
- */
+
 
 char *_strchr(char *s, char c)
 
@@ -27,4 +27,22 @@ s++;
 }
 
 return (found);
+}
+*/
+/**
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
+{
+ unsigned int i = 0;
+
+ for (; *(s + i) != '\0'; i++)
+  if (*(s + i) == c)
+   return (s + i);
+ if (*(s + i) == c)
+  return (s + i);
+ return ('\0');
 }
