d1b54f4c5472ea6af56cd03dd72e76693412ae5a
Gomez7373 <sgc.holberton@gmail.com>
2023-10-31 11:46:45 -0400

    0-puts_recursion.c

--- recursion/main.h
+++ recursion/main.h
@@ -1,7 +1,8 @@
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
-holbertonschool-low_level_programming
+int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+
