915c973bab04ccdefce53a27fe245b4b54a39f21
Gomez7373 <sgc.holberton@gmail.com>
2023-11-01 13:06:38 -0400

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -3,13 +3,16 @@
 #include <stdio.h>
 
 /**
+ * _print_rev_recursion - Prints a tring in revers using recursion.
+ *@s : Pointer to string.
+ *
  */
 
 void _print_rev_recursion(char *s)
 {
- if (*s)
- {
- _print_rev_recursion(s + 1);
- putchar(*s);
- }
+if (*s)
+{
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
 }
