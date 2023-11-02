d0c270b7937b3f44b193925afccb787d18fef7a4
Gomez7373 <sgc.holberton@gmail.com>
2023-11-02 16:21:41 -0400

    4-add.c

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+int total = 0;
+int num; 
+int len;
+int i;
+char *arg;
+if (argc < 2)
+{
+printf("0\n");
+return (0);
+}	
+
+for (i = 1; i < argc; i++)
+{
+arg = argv[i];
+len = 0; 
+
+while (arg[len])
+{
+if (!isdigit(arg[len]) || atoi(arg) < 0)
+{
+printf("Error\n");
+return (1);
+}
+len++;
+}
+num = atoi(arg);
+total += num;
+}
+printf("%d\n", total);
+return (0);
+}
