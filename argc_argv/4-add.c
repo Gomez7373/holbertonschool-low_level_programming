ce9658953cdbaa5c6294d565afba0dddad4b6a93
Gomez7373 <sgc.holberton@gmail.com>
2023-11-02 16:29:06 -0400

    4-add.c

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 /**
- *
- * main - entrery point 
- * @argc: argument count
- * @argv: argument vector
- * return: 0 (Success) or 1 if there is an error.
- */
+* main - entrery point
+* @argc: argument count
+* @argv: argument vector
+* return: 0 (Success) or 1 if there is an error.
+*/
 int main(int argc, char *argv[])
 {
 int total = 0;
@@ -19,13 +18,11 @@ if (argc < 2)
 {
 printf("0\n");
 return (0);
-}	
-
+}
 for (i = 1; i < argc; i++)
 {
 arg = argv[i];
-len = 0; 
-
+len = 0;
 while (arg[len])
 {
 if (!isdigit(arg[len]) || atoi(arg) < 0)
