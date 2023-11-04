417737ccc8babdd0051f4f4a48e0fede7a2a2ea7
Gomez7373 <sgc.holberton@gmail.com>
2023-11-04 15:16:52 -0400

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main  - entry poin 
+ * main - entry poin
  * @argc: argument count
  * @argv: argument vector
  * Return: Always 0 (Success)
@@ -8,8 +8,8 @@
 
 int main(int argc, char *argv[])
 {
-	(void) argv;
-	printf("%d\n", argc -1 );
+(void) argv;
+printf("%d\n", argc -1 );
 
-	return (0);
+return (0);
 }
