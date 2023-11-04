1ca0ab342565f6a26d0eec97acd4bef754d5b4f0
Gomez7373 <sgc.holberton@gmail.com>
2023-11-04 17:59:47 -0400

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,14 +2,12 @@
 
 int factorial(int n);
 
-int main()
-{
-    
+int main() {
+    // Example usage:
     int num = 5;
     int result = factorial(num);
 
-    if (result == -1)
-    {
+    if (result == -1) {
         printf("Error: Negative input\n");
     } else {
         printf("Factorial of %d is %d\n", num, result);
@@ -17,21 +15,19 @@ int main()
 
     return 0;
 }
-/**
-int factorial(int n)
-{
-    
+
+int factorial(int n) {
+    // Check for negative input
     if (n < 0) {
-        return -1; 
+        return -1; // Error: Negative input
     }
 
-    
-    if (n == 0)
-    {
+    // Base case: Factorial of 0 is 1
+    if (n == 0) {
         return 1;
     }
 
-    
+    // Recursive case: n! = n * (n-1)!
     return n * factorial(n - 1);
 }
-*/
+
