815deff8412b749e390380bc5a17013c66cd6ff3
Gomez7373 <sgc.holberton@gmail.com>
2023-11-03 22:25:04 -0400

    2-args.c

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,20 +1,18 @@
-cat 2-args.c
-#include &lt;stdio.h&gt;
+#include <stdio.h>
 /**
-* main - entry point
-* @argc: argument count
-* @argv: argument vector
-*
-* Return: Always 0 (Success)
-*/
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 (Success)
+ */
 int main(int argc, char *argv[])
 {
-if (argc &gt; 0)
+if (argc > 0)
 {
 int i;
-for (i = 0; i &lt; argc; i++)
+for (i = 0; i < argc; i++)
 {
-printf(&quot;%s\n&quot;, argv[i]);
+printf("%s\n", argv[i]);
 }
 }
 return (0);
