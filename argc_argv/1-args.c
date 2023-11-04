7072296684293550f538126212df9b0c4aed85ce
Gomez7373 <sgc.holberton@gmail.com>
2023-11-04 14:21:02 -0400

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -15,25 +15,13 @@ int main(void)
         if (i < 1)
         {
             printf("%i\n", i);
-            i++;
+	    else if i++;
         }
         else
         {
             break;
         }
     }
-for (i = 1; i == 1; argc++)
-{
-	if (i < 2)
-	{
-	printf("%i\n",i);
-	i++;
-	}
-	else
-	{
-	break;
-	}
-}
 
     return 0;
 }
