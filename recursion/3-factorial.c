692ab6e00a06b1b290481f42d30290f6d39d658b
Gomez7373 <sgc.holberton@gmail.com>
2023-11-04 18:31:09 -0400

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
 
-unsigned long factorial(int n);
-
-int main(void)
+int factorial(int n)
 {
-    int number;
-    
-    printf("Enter a number: ");
-    scanf("%d", &number);
-
-    printf("Factorial of %d = %lu\n", number, factorial(number));
-
-    return 0;
-}
-
-unsigned long factorial(int n)
-{
-    if (n == 0 || n == 1)
-        return 1;
+    if (n < 0)
+	    return (-1);
+	 else if  (n == 0)
+        return (1);
     else
         return n * factorial(n - 1);
 }
