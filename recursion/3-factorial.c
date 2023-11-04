9a9877b55f67cf6d8f597d3b7f48a04e02aef1ec
Gomez7373 <sgc.holberton@gmail.com>
2023-11-04 16:12:26 -0400

    3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+
 int factorial(int n);
 
-int main()
-{
-printf("Factorial of 5 is: %d\n", factorial(5));
-if (factorial(-3) == -1) 
-printf("Error: Cannot calculate factorial for a negative number\n");
+int main() {
+    printf("Factorial of 5 is: %d\n", factorial(5));
+    
+    if (factorial(-3) == -1) 
+        printf("Error: Cannot calculate factorial for a negative number\n");
 
-return (0);
+    return 0;
 }
+
