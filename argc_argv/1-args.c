bdee2309651bea3a8876805fb04804b0eeb9f03a
Gomez7373 <sgc.holberton@gmail.com>
2023-11-04 14:13:54 -0400

    1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
+
 /**
- * main - entry poin
- * @argc: argument count
- * @argv: argument value
+ * main - entry point
  * Return: Always 0
  */
 
 int main(void)
 {
-int argc;
-int i;
+    int argc = 0;
+    int i;
 
-argc = 0;
-i = 0;
-{
-printf("%d\n", argc);
+    printf("%d\n", argc);
+
+    for (i = 0; i == 0; argc++)
+    {
+        if (i < 1)
+        {
+            printf("%i\n", i);
+            i++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return 0;
 }
 
-for (i = 0; i  == 0; argc++)
-{
-	if (i < 1))
-{
-	printf("%i0\n", i);
-	i++;
-}
-else
-{
-	break;
-}
-}
-
- 
-return (0);
-}
