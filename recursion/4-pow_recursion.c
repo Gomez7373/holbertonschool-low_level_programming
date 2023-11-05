14329f27c124b9be7fa50624fd758d1b01b5d3d0
Gomez7373 <sgc.holberton@gmail.com>
2023-11-04 21:32:36 -0400

    4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -7,17 +7,17 @@
  * Return: The result of x raised to the power of y.
  *         If y is lower than 0, return -1.
  */
-int _pow_recursion(int x, int y) {
-
+int _pow_recursion(int x, int y)
+{
 if (y < 0)
 {
-return -1;
+return (-1);
 }
 
-if (y == 0) {
-return 1;
+if (y == 0)
+{
+return (1);
 }
 
-
-return x * _pow_recursion(x, y - 1);
+return (x * _pow_recursion(x, y - 1));
 }
