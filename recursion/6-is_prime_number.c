cc0aa25f257bb8cd1bcedbc65ab728dfc680949c
Gomez7373 <sgc.holberton@gmail.com>
2023-11-05 15:54:02 -0400

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,13 +10,12 @@ int is_prime_number(int n)
 {
 int i;
 int sqrt;
-sqrt = sqrt(n);
 
 if (n <= 1)
 {
 return (0);
 }
-for (i = 2; i <= sqrt(n); i++)
+for (i = 2; i <= sqrt; i++)
 {
 if (n % i == 0)
 {
