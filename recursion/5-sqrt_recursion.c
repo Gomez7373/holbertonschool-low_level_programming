5efa7ac8a2508d35bead2af5b37d89611f31d99c
Gomez7373 <sgc.holberton@gmail.com>
2023-11-05 15:19:37 -0400

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,31 +1,35 @@
 #include "main.h"
+/**
+ *
+ */
+
 
 int _sqrt_recursion_helper(int n, int i)
 {
-    int result;
+int result;
 
-    if (n == 0 || n == 1 || i * i > n)
-    {
-        return 1;
-    }
+if (n == 0 || n == 1 || i * i > n)
+{
+return (1);
+}
 
-    if (i * i == n)
-    {
-        return i;
-    }
+if (i * i == n)
+{
+return (i);
+}
 
-    result = _sqrt_recursion_helper(n, i + 1);
-    return result;
+result = _sqrt_recursion_helper(n, i + 1);
+return (result);
 }
 
 int _sqrt_recursion(int n)
 {
-	int result;
-    if (n < 0)
-    {
-	    return -1;
-    }
-   result = _sqrt_recursion_helper(n, 1);
-    
-   return result;  
+int result;
+if (n < 0)
+{
+return (-1);
+}
+result = _sqrt_recursion_helper(n, 1);
+
+return (result);  
 }
