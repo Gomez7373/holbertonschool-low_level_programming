4688dcf59c068e62beff8c02e4c9ea2f0554bd10
Gomez7373 <sgc.holberton@gmail.com>
2023-11-08 19:58:50 -0400

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,29 +1,34 @@
 #include <stdlib.h>
 
-char *_strdup(char *str) {
-    if (str == NULL) {
-        return (NULL);
-    }
+char *_strdup(char *str)
+{
+if (str == NULL)
+{
+return (NULL);
+}
 
-    char *duplicate;
-    unsigned int length = 0;
+char *duplicate;
+unsigned int length = 0;
 
-    // Calculate the length of the input string
-    while (str[length] != '\0') {
-        length++;
-    }
 
-    // Allocate memory for the duplicate string
-    duplicate = (char *)malloc((length + 1) * sizeof(char));
+while (str[length] != '\0')
+{
+length++;
+}
 
-    if (duplicate == NULL) {
-        return (NULL); 
-    }
 
-    for (unsigned int i = 0; i <= length; i++) {
-        duplicate[i] = str[i];
-    }
+duplicate = (char *)malloc((length + 1) * sizeof(char));
 
-    return (duplicate);
+if (duplicate == NULL)
+{
+return (NULL); 
+}
+
+for (unsigned int i = 0; i <= length; i++) 
+{
+duplicate[i] = str[i];
+}
+
+return (duplicate);
 }
 
