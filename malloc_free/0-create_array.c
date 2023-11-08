090f9fdbd7706b1787c77e61c396831f76433873
Gomez7373 <sgc.holberton@gmail.com>
2023-11-08 16:20:05 -0400

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,32 +1,23 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include "main.h"
 
-/**
- *
- */
+char *create_array(unsigned int size, char c) {
+    if (size == 0) {
+        return NULL;
+    }
 
-char *create_array(unsigned int size, char c)
-{
-       int array;
+    char *array;
+    unsigned int i;
 
-if (size == 0)
-{
-	return (NULL);
-}
-{
-	char *array;
-	unsigned int i;
+    array = (char *)malloc(size * sizeof(char));
 
-	array =(char *)malloc(size * sizeof (char));
-	if (array == NULL)
-{
-	return (NULL);
+    if (array == NULL) {
+        return NULL; // Allocation failed
+    }
+
+    for (i = 0; i < size; i++) {
+        array[i] = c;
+    }
+
+    return array;
 }
-	for (i = 0 ; i < size; i++)
-{
-	array [i] = c;
-}
-}
-	return (*array);
-}
+
