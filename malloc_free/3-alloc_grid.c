88bb5baa1d2e624ab9857c1ba80ad4f8578271f3
Gomez7373 <sgc.holberton@gmail.com>
2023-11-09 16:34:29 -0400

    3-alloc_grid.c

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+/**
+ * alloc_grind - Allocate a 2D array of intergers.
+ * @width: The width of the grid.
+ * @height: The height of the grid.
+ *
+ * Return: Apointer to the newly allocated 2D array, or NULL on failure. 
+ */
+int **alloc_grind(int widrh, int height)
+{
+int **grid;
+int i, j;
+if (widhth <= 0 || height <= 0)
+	return (NULL);
+
+grid = (int **)malloc(height * sizeof(int *))
+
+	if (grid == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+
+grid[i] = (int *)malloc(width * sizeof (int));
+
+	if (grind[i] == NULL)
+	{
+	for (j = 0; j < i; j++)
+
+		free(grid[j]);
+	return (NULL);
+	}
+for (j = 0; j < width; j++)
+	gtid[i][j] = 0;
+	}
+return (grind);
+}
