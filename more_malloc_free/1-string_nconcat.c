eaa5a405e007a8a92166fc9c2580897f59e91d8a
Gomez7373 <sgc.holberton@gmail.com>
2023-11-09 17:42:15 -0400

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: Size of the memory to be allocated
+ *
+ * Return: Pointer to the allocated memory
+ *         Exits with status 98 if malloc fails
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+
+if (ptr == NULL)
+{
+exit(98);
+}
+
+return (ptr);
+}
