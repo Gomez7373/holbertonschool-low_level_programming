a6b1611529bd8b13d1fa06d4478f35af5929f441
Gomez7373 <sgc.holberton@gmail.com>
2023-11-10 13:01:21 -0400

    file name

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return 0;
+printf("%s\n", __FILE__);
+return 0;
 }
-
