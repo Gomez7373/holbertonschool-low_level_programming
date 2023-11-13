8a2c3eaaf23affcecb9a07a5185e8b3473efe19c
Gomez7373 <sgc.holberton@gmail.com>
2023-11-13 17:22:03 -0400

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,10 +7,11 @@
 *@owner: The oener of the dog.
 */
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+struct dog
+{
+char *name;
+float age;
+char *owner;
 };
 
 #endif /* DOG_H */
