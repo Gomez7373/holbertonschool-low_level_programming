89e2089174f36ee58cf79dac1954b427cb185627
Gomez7373 <sgc.holberton@gmail.com>
2023-11-15 22:44:03 -0400

    file name

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,30 +16,33 @@ dog_t *new_dog(char *name, float age, char *owner)
 
     if (name == NULL || owner == NULL)
     {
-        return NULL;
+        return (NULL);
     }
 
     new_dog = malloc(sizeof(dog_t));
 
     if (new_dog == NULL)
     {
-        return NULL;
+        return (NULL);
     }
 
     new_dog->name = strdup(name);
-    new_dog->owner = strdup(owner);
-
-    if (new_dog->name == NULL || new_dog->owner == NULL)
+    if (new_dog->name == NULL)
     {
-        
-        free(new_dog->name);
-        free(new_dog->owner);
         free(new_dog);
-        return NULL;
+        return (NULL);
+    }
+
+    new_dog->owner = strdup(owner);
+    if (new_dog->owner == NULL)
+    {
+        free(new_dog->name);
+        free(new_dog);
+        return (NULL);
     }
 
     new_dog->age = age;
 
-    return new_dog;
+    return (new_dog);
 }
 
