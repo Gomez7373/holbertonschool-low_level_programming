2c78871723aeb3888b6951cce5a0eee809460925
Gomez7373 <sgc.holberton@gmail.com>
2023-11-19 17:45:33 -0400

    prints

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -30,11 +30,13 @@ va_start(args, format);
 
 while (format && format[i])
 {
-if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's') &&
+if ((format[i] == 'c' || format[i] == 'i' || format[i]
+		       	== 'f' || format[i] == 's') &&
 (format[i] == 'c' ? printf("%c", va_arg(args, int)), 1 :
 format[i] == 'i' ? printf("%d", va_arg(args, int)), 1 :
 format[i] == 'f' ? printf("%f", va_arg(args, double)), 1 :
-format[i] == 's' ? (str = va_arg(args, char *)) ? printf("%s", str) : printf("(nil)"), 1 : 0) &&
+format[i] == 's' ? (str = va_arg(args, char *))
+? printf("%s", str) : printf("(nil)"), 1 : 0) &&
 format[i + 1] != '\0')
 {
 printf(", ");
