76e13c9db5a110dab3a35077d83d1fe2e2a3b09f
Gomez7373 <sgc.holberton@gmail.com>
2023-11-28 17:14:07 -0400

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,34 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stddef.h>
-#include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: A pointer to the head of the list_t.
- *
- * Return: The number of nodes in the list.
- */
-
-size_t print_list(const list_t *h)
-
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
+typedef struct list_s
 {
+char *str;
+unsigned int len;
+struct list_s *next;
+} list_t;
 
-size_t count = 0;
+/* Function prototypes */
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-while (h != NULL)
+#endif // LISTS_H
 
-{
-
-if (h->str == NULL)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-count++;
-
-}
-
-return (count);
-}
