0b351580aa24438aa81bf77d70e166eb85fe62d5
Gomez7373 <sgc.holberton@gmail.com>
2023-11-28 15:41:20 -0400

    README

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#define LIST_H
+#ifdef LIST_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+
+
+
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+
+#endif /*LIST.H*/
