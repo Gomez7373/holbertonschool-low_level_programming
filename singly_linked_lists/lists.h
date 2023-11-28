43155850053501df3124b164204997adf829804c
Gomez7373 <sgc.holberton@gmail.com>
2023-11-28 17:00:57 -0400

    README

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,17 +1,24 @@
 #define LIST_H
 #ifdef LIST_H
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <string.h>
+#include <stddef.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
 
-
-
+/* Function prototypes */
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
