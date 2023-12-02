1f2b8ffa408450a874515d41d4609abe1a04565d
Gomez7373 <sgc.holberton@gmail.com>
2023-12-02 00:41:38 -0400

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,39 +1,54 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given
- * index in a doubly linked list.
- * @h: A pointer to the head of the doubly linked list.
- * @idx: The index where the new node should be inserted.
- * @n: The data to be stored in the new node.
- * Return: The address of the new node, or NULL if it failed.
- */
+* insert_dnodeint_at_index - Inserts a new node at a
+* given position in a doubly linked list.
+* @h: A pointer to the head of the doubly linked list.
+* @idx: The index where the new node should be inserted.
+* @n: The data to be stored in the new node.
+* Return: The address of the new node, or NULL if it failed.
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *temp;
+dlistint_t *new_node, *temp = *h;
+unsigned int i;
 
-	if (!h)
-		return (NULL);
+if (!h)
+return (NULL);
 
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
+new_node = malloc(sizeof(dlistint_t));
+if (!new_node)
+return (NULL);
 
-	new->n = n;
-	if (!idx)
-		return (new->next = *h, new->prev = NULL, (*h) ? (*h)->prev = new : NULL, *h = new);
+new_node->n = n;
 
-	for (temp = *h; idx-- && temp; temp = temp->next)
-		continue;
-
-	if (idx || !(new->prev = temp))
-	{
-		free(new);
-		return (NULL);
-	}
-
-	return (new->next = temp->next, temp->next = (temp->next) ? (temp->next)->prev = new : NULL, new);
+if (idx == 0)
+{
+new_node->prev = NULL;
+new_node->next = *h;
+if (*h)
+	(*h)->prev = new_node;
+*h = new_node;
+return (new_node);
 }
 
+for (i = 0; i < idx - 1 && temp; i++)
+temp = temp->next;
+
+if (!temp)
+{
+free(new_node);
+return (NULL);
+}
+
+new_node->prev = temp;
+new_node->next = temp->next;
+
+if (temp->next)
+temp->next->prev = new_node;
+
+temp->next = new_node;
+
+return (new_node);
+}
