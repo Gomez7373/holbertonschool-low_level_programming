46298c6df4b987a1cae6eb6b6451291ef22577d7
Gomez7373 <sgc.holberton@gmail.com>
2023-12-02 14:51:17 -0400

    2-get_bit.c

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -8,7 +8,8 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
-if (index >= sizeof(unsigned long int) * 8)  /* Check if index is out of bounds*/
+if (index >= sizeof(unsigned long int) * 8) 
+/* Check if index is out of bounds*/
 return (-1);
 
 return ((n >> index) & 1);
