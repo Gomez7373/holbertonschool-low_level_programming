7bfee629e6e6a69b6e860a54a5dab19d2b374665
Gomez7373 <sgc.holberton@gmail.com>
2023-12-02 00:38:11 -0400

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,37 +3,37 @@
 #include <stdlib.h>
 
 /**
-* insert_dnodeint_at_index - Inserts a new node at a given
-* index in a doubly linked list.
-* @h: A pointer to the head of the doubly linked list.
-* @idx: The index where the new node should be inserted.
-* @n: The data to be stored in the new node.
-* Return: The address of the new node, or NULL if it failed.
-*/
+ * insert_dnodeint_at_index - Inserts a new node at a given
+ * index in a doubly linked list.
+ * @h: A pointer to the head of the doubly linked list.
+ * @idx: The index where the new node should be inserted.
+ * @n: The data to be stored in the new node.
+ * Return: The address of the new node, or NULL if it failed.
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *new, *temp;
+	dlistint_t *new, *temp;
 
-    if (!h)
-        return (NULL);
+	if (!h)
+		return (NULL);
 
-    new = malloc(sizeof(dlistint_t));
-    if (!new)
-        return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 
-    new->n = n;
-    if (!idx)
-        return (new->next = *h, new->prev = NULL, (*h) ? (*h)->prev = new : NULL, *h = new);
+	new->n = n;
+	if (!idx)
+		return (new->next = *h, new->prev = NULL, (*h) ? (*h)->prev = new : NULL, *h = new);
 
-    for (temp = *h; idx-- && temp; temp = temp->next)
-        continue;
+	for (temp = *h; idx-- && temp; temp = temp->next)
+		continue;
 
-    if (idx || !(new->prev = temp))
-    {
-        free(new);
-        return (NULL);
-    }
+	if (idx || !(new->prev = temp))
+	{
+		free(new);
+		return (NULL);
+	}
 
-    return (new->next = temp->next, temp->next = (temp->next) ? (temp->next)->prev = new : NULL, new);
+	return (new->next = temp->next, temp->next = (temp->next) ? (temp->next)->prev = new : NULL, new);
 }
 
