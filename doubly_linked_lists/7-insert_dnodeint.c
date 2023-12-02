ef5a5a0cb0c33e6e971562f663bc05908b55100c
Gomez7373 <sgc.holberton@gmail.com>
2023-12-02 00:08:59 -0400

    7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -3,76 +3,49 @@
 #include <stdlib.h>
 
 /**
-* insert_at_beginning - Inserts a new node at the beginning of a dlistint_t list.
-*/
-dlistint_t *insert_at_beginning(dlistint_t **h, dlistint_t *new_node)
-{
-new_node->prev = NULL;
-new_node->next = *h;
-if (*h != NULL)
-(*h)->prev = new_node;
-*h = new_node;
-return (new_node);
-}
-
-/**
-* traverse_to_insertion_point - Traverses a dlistint_t list to the insertion point.
-*/
-dlistint_t *traverse_to_insertion_point(dlistint_t **h, unsigned int idx)
-{
-dlistint_t *temp = *h;
-unsigned int count = 0;
-
-while (temp != NULL && count < idx)
-{
-temp = temp->next;
-count++;
-}
-
-return (temp);
-}
-
-/**
-* update_pointers_for_insertion - Updates pointers for inserting a new node.
-*/
-void update_pointers_for_insertion(dlistint_t *temp, dlistint_t *new_node)
-{
-new_node->prev = temp;
-new_node->next = temp->next;
-
-if (temp->next != NULL)
-temp->next->prev = new_node;
-
-temp->next = new_node;
-}
-
-/**
-* insert_dnodeint_at_index - Inserts a new node at a given position in a dlistint_t list.
+* insert_dnodeint_at_index - Inserts a new node at a given
+* index in a doubly linked list.
+* @h: A pointer to the head of the doubly linked list.
+* @idx: The index where the new node should be inserted.
+* @n: The data to be stored in the new node.
+* Return: The address of the new node, or NULL if it failed.
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *temp;
+dlistint_t *new, *temp = *h;
 
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
+if (!h)
+return (NULL);
+
+new = malloc(sizeof(dlistint_t));
+if (!new)
+return (NULL);
+
+new->n = n;
+
+if (!idx)
 {
-perror("Error: Unable to allocate memory for new node");
+new->prev = NULL, new->next = *h;
+if (*h)
+(*h)->prev = new;
+return (*h = new);
+}
+
+for (; idx && temp; idx--)
+temp = temp->next;
+
+if (idx || !temp)
+{
+free(new);
 return (NULL);
 }
 
-new_node->n = n;
+new->prev = temp, new->next = temp->next;
+if (temp->next)
+(temp->next)->prev = new;
 
-if (idx == 0)
-return (insert_at_beginning(h, new_node));
+temp->next = new;
 
-temp = traverse_to_insertion_point(h, idx - 1);
-if (temp == NULL)
-{
-free(new_node);
-return (NULL);
+return (new);
 }
 
-update_pointers_for_insertion(temp, new_node);
-
-return (new_node);
-}
