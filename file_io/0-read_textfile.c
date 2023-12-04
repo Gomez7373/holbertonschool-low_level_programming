dffa14cf2b7116248bdc6727797c44c1755d165a
Gomez7373 <sgc.holberton@gmail.com>
2023-12-04 13:19:24 -0400

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,7 +9,8 @@
 * Return: The actual number of letters read and printed. If an error occurs,
 * return 0.
 */
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 int fd;
 char *buffer;
 ssize_t bytes_read;
@@ -23,20 +24,23 @@ if (fd == -1)
 return 0;
 
 buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL) {
+if (buffer == NULL)
+{
 close(fd);
 return 0;
 }
 
 bytes_read = read(fd, buffer, letters);
-if (bytes_read == -1) {
+if (bytes_read == -1)
+{
 close(fd);
 free(buffer);
 return 0;
 }
 
 bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-if (bytes_written == -1 || bytes_written != bytes_read) {
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
 close(fd);
 free(buffer);
 return 0;
