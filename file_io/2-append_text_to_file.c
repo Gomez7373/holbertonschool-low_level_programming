d677e2cef7ae8df38e7fe3a56ebf42ef9bd73dd3
Gomez7373 <sgc.holberton@gmail.com>
2023-12-08 11:13:45 -0400

    2-append_text_to_file.c

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 if (filename == NULL)
 return (-1);
 
-int file_descriptor = open(filename, O_WRONLY | O_APPEND);
+int file_descriptor = open(filename, O_WRONLY | O_APPEND)
 
 if (file_descriptor == -1)
 return (-1);  /* File couldn't be opened or doesn't exist */
