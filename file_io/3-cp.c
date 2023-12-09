40190552c71d5f9b0ed7f25143ef2374a74c4133
Gomez7373 <sgc.holberton@gmail.com>
2023-12-08 20:40:31 -0400

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -14,33 +14,33 @@ int main(int argc, char *argv[])
 
     if (argc != 3)
     {
-        dprintf(2, "Usage: %s file_from file_to\n", argv[0]);
+        dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
         return 97;
     }
 
     fd_from = open(argv[1], O_RDONLY);
     if (fd_from == -1)
-        dprintf(98, "Error: Can't read from file %d\n", int);
+        dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd_from);
 
     fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (fd_to == -1)
-        dprintf(99, "Error: Can't write to %d\n", int);
+        dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd_to);
 
     while ((n_read = read(fd_from, buffer, sizeof(buffer))) > 0)
     {
         n_write = write(fd_to, buffer, n_read);
         if (n_write == -1 || n_write != n_read)
-           dprintf(99, "Error: Can't write to %d\n", int);
+           dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd_to);
     }
 
     if (n_read == -1)
-        dprintf(98, "Error: Can't read from file %d\n", int);
+        dprintf(STDERR_FILENO, "Error: Can't read from file %d\n", fd_from);
 
     if (close(fd_from) == -1)
-        dprintf(100, "Error: Can't close fd %d\n", fd_from);
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 
     if (close(fd_to) == -1)
-       dprintf(100, "Error: Can't close fd %d\n", fd_to);
+       dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 
     return 0;
 }
