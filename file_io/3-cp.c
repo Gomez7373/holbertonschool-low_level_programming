eb0c5faa082cd0e0cb88d58a5f86ec7cc2c6d60a
Gomez7373 <sgc.holberton@gmail.com>
2023-12-09 11:21:57 -0400

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,45 +1,46 @@
 #include "main.h"
-
-void print_error(const char *msg, const char *file) {
-    dprintf(STDERR_FILENO, "Error: %s %s\n", msg, file);
+void print_error(int code, const char *format, int arg)
+{
+dprintf(STDERR_FILENO, "Error: ");
+dprintf(STDERR_FILENO, format, arg);
+dprintf(STDERR_FILENO, "\n");
+exit(code);
 }
-
-int main(int argc, char *argv[]) {
-    int file_from = open(argv[1], O_RDONLY);
-    if (argc != 3 || file_from == -1) {
-        if (argc != 3)
-            dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        if (file_from == -1)
-            print_error("Can't read from file", argv[1]);
-        exit(argc != 3 ? 97 : 98);
-    }
-
-    int file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-    if (file_to == -1) {
-        print_error("Can't write to", argv[2]);
-        close(file_from);
-        exit(99);
-    }
-
-    char buffer[1024];
-    ssize_t num1, num2;
-
-    while ((num1 = read(file_from, buffer, 1024)) > 0) {
-        num2 = write(file_to, buffer, num1);
-        if (num2 < num1) {
-            print_error("Can't write to", argv[2]);
-            close(file_from);
-            close(file_to);
-            exit(99);
-        }
-    }
-
-    if (num1 == -1)
-        print_error("Can't read from file", argv[1]);
-    
-    close(file_from);
-    close(file_to);
-
-    return num1 == -1 ? 98 : 0;
+int main(int argc, char *argv[])
+{
+int file_from, file_to, num1, num2;
+char buffer[1024];
+if (argc != 3)
+{
+print_error(97, "Usage: cp file_from file_to\n", 0);
 }
-
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+{
+print_error(98, "Can't read from file %s", 0);
+}
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+if (file_to == -1)
+{
+close(file_from);
+print_error(99, "Can't write to %s", 0);
+}
+while ((num1 = read(file_from, buffer, 1024)) > 0)
+{
+num2 = write(file_to, buffer, num1);
+if (num2 < num1)
+{
+close(file_from);
+close(file_to);
+print_error(99, "Can't write to %s", 0);
+}
+}
+if (num1 == -1)
+{
+print_error(98, "Can't read from file %s", 0);
+}
+close(file_from);
+close(file_to);
+return (num1 == -1 ? 98 : 0);
+}
