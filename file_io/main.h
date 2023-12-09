867613e33249cfca1a7ba3cef3f0d373e70e55c6
Gomez7373 <sgc.holberton@gmail.com>
2023-12-09 11:43:31 -0400

    3-cp.c

--- file_io/main.h
+++ file_io/main.h
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content);
 
 /*void print_error(int code, const char *format, const char *arg);*/
 
-
 void print_error(int code, const char *format, ...);
+
 #endif /* MAIN_H */
 
