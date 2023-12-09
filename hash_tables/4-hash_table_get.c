678ee4c435b1107e26b5228ccce0714ac77c10b5
Gomez7373 <sgc.holberton@gmail.com>
2023-12-09 10:52:36 -0400

    4-hash_table_get.c

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -6,7 +6,8 @@
 * @ht: hash table to look into
 * @key: key to search for
 *
-* Return: the value associated with the element, or NULL if key couldn't be found.
+* Return: the value associated with the element,
+* or NULL if key couldn't be found.
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
