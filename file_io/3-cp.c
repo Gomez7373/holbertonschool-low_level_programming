a4def87359dd78e827a894f08c6fe302e363c662
Gomez7373 <sgc.holberton@gmail.com>
2023-12-09 11:16:29 -0400

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,37 +1,45 @@
 #include "main.h"
 
-#define BUFF_SIZE 1024
-
-void error_exit(int code, const char *msg, const char *file_name)
-{
-dprintf(STDERR_FILENO, msg, file_name);
-exit(code);
+void print_error(const char *msg, const char *file) {
+    dprintf(STDERR_FILENO, "Error: %s %s\n", msg, file);
 }
 
-int main(int argc, char *argv[])
-{
-int file_from, file_to, rd, wr;
-char buffer[BUFF_SIZE];
+int main(int argc, char *argv[]) {
+    int file_from = open(argv[1], O_RDONLY);
+    if (argc != 3 || file_from == -1) {
+        if (argc != 3)
+            dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        if (file_from == -1)
+            print_error("Can't read from file", argv[1]);
+        exit(argc != 3 ? 97 : 98);
+    }
 
-if (argc != 3)
-error_exit(97, "Usage: cp file_from file_to\n", "");
+    int file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    if (file_to == -1) {
+        print_error("Can't write to", argv[2]);
+        close(file_from);
+        exit(99);
+    }
 
-file_from = open(argv[1], O_RDONLY);
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    char buffer[1024];
+    ssize_t num1, num2;
 
-if (file_from == -1 || file_to == -1)
-error_exit(98, "Error: Can't read from file %s\n",
-file_from == -1 ? argv[1] : argv[2]);
+    while ((num1 = read(file_from, buffer, 1024)) > 0) {
+        num2 = write(file_to, buffer, num1);
+        if (num2 < num1) {
+            print_error("Can't write to", argv[2]);
+            close(file_from);
+            close(file_to);
+            exit(99);
+        }
+    }
 
-while ((rd = read(file_from, buffer, BUFF_SIZE)) > 0)
-if ((wr = write(file_to, buffer, rd)) == -1 || wr != rd)
-error_exit(99, "Error: Can't write to %s\n", argv[2]);
+    if (num1 == -1)
+        print_error("Can't read from file", argv[1]);
+    
+    close(file_from);
+    close(file_to);
 
-if (rd == -1)
-error_exit(98, "Error: Can't read from file %s\n", argv[1]);
-
-close(file_from);
-close(file_to);
-
-return (0);
+    return num1 == -1 ? 98 : 0;
 }
+
