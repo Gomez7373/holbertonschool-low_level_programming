39a8594078f1545c180b4b07da0d6c8d4e867ea3
Gomez7373 <sgc.holberton@gmail.com>
2023-12-09 10:25:04 -0400

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -9,27 +9,27 @@
  * Return: 1 if it succeeded, 0 otherwise.
  * In case of collision, adds the new node at the beginning of the list.
 */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
 unsigned long int index;
-hash_node_t *new_node, *temp;
-if (!ht || !key || !*key || !value)
+hash_node_t *new_node, *temp; if (!ht || !key || !*key || !value)
 return (0);
 index = key_index((unsigned char *)key, ht->size);
-temp = ht->array[index];
-while (temp) {
-if (strcmp(temp->key, key) == 0) {
+temp = ht->array[index]; while (temp)
+{
+if (strcmp(temp->key, key) == 0)
+{
 free(temp->value);
 temp->value = strdup(value);
 return (1);
 }
 temp = temp->next;
 }
-new_node = malloc(sizeof(hash_node_t));
-if (!new_node)
+new_node = malloc(sizeof(hash_node_t)); if (!new_node)
 return (0);
 new_node->key = strdup(key);
-new_node->value = strdup(value);
-if (!new_node->key || !new_node->value) {
+new_node->value = strdup(value); if (!new_node->key || !new_node->value)
+{
 free(new_node->key);
 free(new_node);
 return (0);
