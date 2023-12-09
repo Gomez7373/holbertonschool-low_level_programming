a01f31b972c3a180f587d427e7bd1802c039bfcd
Gomez7373 <sgc.holberton@gmail.com>
2023-12-09 11:31:45 -0400

    3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,46 +1,55 @@
 #include "main.h"
-void print_error(int code, const char *format, int arg)
-{
-dprintf(STDERR_FILENO, "Error: ");
-dprintf(STDERR_FILENO, format, arg);
-dprintf(STDERR_FILENO, "\n");
-exit(code);
+
+/**
+ * print_error - prints error message to STDERR
+ * @code: error code
+ * @format: error message format
+ * @arg: argument for format
+ */
+void print_error(int code, const char *format, const char *arg) {
+    dprintf(STDERR_FILENO, format, arg);
+    dprintf(STDERR_FILENO, "\n");
+    exit(code);
 }
-int main(int argc, char *argv[])
-{
-int file_from, file_to, num1, num2;
-char buffer[1024];
-if (argc != 3)
-{
-print_error(97, "Usage: cp file_from file_to\n", 0);
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 Success
+ */
+int main(int argc, char *argv[]) {
+    int file_from, file_to, num1 = 1024, num2 = 0;
+    char buffer[1024];
+
+    if (argc != 3)
+        print_error(97, "Usage: cp file_from file_to", NULL);
+
+    file_from = open(argv[1], O_RDONLY);
+    if (file_from == -1)
+        print_error(98, "Can't read from file %s", argv[1]);
+
+    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    if (file_to == -1)
+        print_error(99, "Can't write to %s", argv[2]);
+
+    while (num1 == 1024) {
+        num1 = read(file_from, buffer, 1024);
+        if (num1 == -1)
+            print_error(98, "Can't read from file %s", argv[1]);
+
+        num2 = write(file_to, buffer, num1);
+        if (num2 < num1)
+            print_error(99, "Can't write to %s", argv[2]);
+    }
+
+    if (close(file_from) == -1)
+        print_error(100, "Can't close fd %d", NULL);
+
+    if (close(file_to) == -1)
+        print_error(100, "Can't close fd %d", NULL);
+
+    return (0);
 }
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
-{
-print_error(98, "Can't read from file %s", 0);
-}
-file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (file_to == -1)
-{
-close(file_from);
-print_error(99, "Can't write to %s", 0);
-}
-while ((num1 = read(file_from, buffer, 1024)) > 0)
-{
-num2 = write(file_to, buffer, num1);
-if (num2 < num1)
-{
-close(file_from);
-close(file_to);
-print_error(99, "Can't write to %s", 0);
-}
-}
-if (num1 == -1)
-{
-print_error(98, "Can't read from file %s", 0);
-}
-close(file_from);
-close(file_to);
-return (num1 == -1 ? 98 : 0);
-}
+
--- file_io/main.h
+++ file_io/main.h
@@ -16,7 +16,9 @@ int append_text_to_file(const char *filename, char *text_content);
 
 int create_file(const char *filename, char *text_content);
 
-void print_error(int code, const char *format, int arg);
+/*void print_error(int code, const char *format, int arg);*/
+
+void print_error(int code, const char *format, const char *arg);
 
 #endif /* MAIN_H */
 
