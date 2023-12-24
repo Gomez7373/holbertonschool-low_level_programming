d12d6f177819e37789b6985af7049baf452c040c
Gomez7373 <sgc.holberton@gmail.com>
2023-12-23 22:20:01 -0400

    102-print_comb5

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+
+/* Function prototype */
+void print_combinations(void);
+
+/**
+* main - Entry point of the program.
+*
+* Return: Always 0 (success).
+*/
+int main(void)
+{
+/* Call the function to print combinations */
+print_combinations();
+return (0);
+}
+
+/**
+* print_combinations - Prints all possible combinations of two two-digit numbers.
+*
+* Description:
+* The function uses nested loops to iterate through all possible combinations
+* of two two-digit numbers. It ensures that the combinations are printed in
+* ascending order and follows the specified formatting rules. The putchar
+* function is used to output the numbers and separators.
+*/
+void print_combinations(void)
+{
+int i, j;
+
+for (i = 0; i <= 99; ++i)
+{
+for (j = i + 1; j <= 99; ++j)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+
+if (!(i == 98 && j == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+}
