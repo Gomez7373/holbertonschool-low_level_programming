7d72d3a01db29e9421f5ac1a1bad2faf2cbb8ea1
Gomez7373 <sgc.holberton@gmail.com>
2023-12-31 16:17:41 -0400

    .

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
+
+/**
+* premain - Function to print lines before main
+*/
 void __attribute__ ((constructor)) premain()
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
-
