77ed6fff03346ed0f46e93352ac8e53b98548bad
Gomez7373 <sgc.holberton@gmail.com>
2024-01-17 17:16:42 -0400

    1-binary

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -9,11 +9,9 @@
 *
 * Return: Index where the value is located, or -1 if not found
 */
-
 int binary_search(int *array, size_t size, int value)
-
 {
-size_t left, right, mid;
+size_t left, right, mid, i;
 
 if (array == NULL)
 return (-1);
@@ -26,7 +24,7 @@ while (left <= right)
 mid = (left + right) / 2;
 
 printf("Searching in array: ");
-for (size_t i = left; i <= right; i++)
+for (i = left; i <= right; i++)
 {
 if (i == right)
 printf("%d\n", array[i]);
@@ -43,6 +41,5 @@ else
 right = mid - 1;
 }
 
-return (-1); /*Value not found*/
+return (-1); /* Value not found */
 }
-
